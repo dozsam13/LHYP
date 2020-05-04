fb9bcd58b64da88fea131e67f4d40a5877750cab
Máté Dózsa <dozsam13@gmail.com>
2020-05-04 10:07:06 +0200

    preprocess

--- hypertrophy_dataset.py
+++ hypertrophy_dataset.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+from torchvision import transforms
 import torch
 
 class HypertrophyDataset(Dataset):
@@ -6,6 +7,12 @@ class HypertrophyDataset(Dataset):
         self.images = images
         self.targets = targets
         self.device = device
+        self.preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
     
     def __len__(self):
         return len(self.images)
