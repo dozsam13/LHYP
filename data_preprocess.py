bd873e7a61a94c41175bd10560bd2f7adb9a707d
dozsam13 <dozsam13@gmail.com>
2020-08-04 11:39:05 +0200

    data preprocess error handling

--- data_preprocess.py
+++ data_preprocess.py
@@ -10,17 +10,20 @@ import cv2 as cv
 
 logger = get_logger(__name__)
 
+
 def create_path_for_file(pickle_file_path):
     os.makedirs(os.path.dirname(pickle_file_path), exist_ok=True)
+
 
 def collect_contour_slices_by_frames(contours):
     frameSliceDict = {}
     for slc in contours:
         for frm in contours[slc]:
-            if not(frm in frameSliceDict):
+            if not (frm in frameSliceDict):
                 frameSliceDict[frm] = []
             frameSliceDict[frm].append(slc)
     return frameSliceDict
+
 
 def left_ventricle_contours(contours):
     left_ventricle_color_modes = {"ln", "lp"}
@@ -30,10 +33,11 @@ def left_ventricle_contours(contours):
             filtered_contours = dict(filter(lambda contour: contour[0] in left_ventricle_color_modes, modes.items()))
             if len(filtered_contours) == 0:
                 continue
-            if not(slc in left_ventricle_contours):
+            if not (slc in left_ventricle_contours):
                 left_ventricle_contours[slc] = {}
             left_ventricle_contours[slc][frm] = filtered_contours
     return left_ventricle_contours
+
 
 def order_frames(frame_slice_dict, contours):
     frame1 = list(frame_slice_dict.keys())[0]
@@ -43,9 +47,10 @@ def order_frames(frame_slice_dict, contours):
 
     slice_intersection = list(set(slice_dict_1).intersection(set(slice_dict_2)))
     slice_intersection.sort()
-    mid_slice_index = slice_intersection[len(slice_intersection)//2]
+    mid_slice_index = slice_intersection[len(slice_intersection) // 2]
 
-    common_contour_mode = next(iter(set(contours[mid_slice_index][frame1].keys()).intersection(contours[mid_slice_index][frame2])))
+    common_contour_mode = next(
+        iter(set(contours[mid_slice_index][frame1].keys()).intersection(contours[mid_slice_index][frame2])))
 
     area1 = cv.contourArea(contours[mid_slice_index][frame1][common_contour_mode].astype(int))
     area2 = cv.contourArea(contours[mid_slice_index][frame2][common_contour_mode].astype(int))
@@ -55,16 +60,17 @@ def calculate_mid_slice(frame_slice_dict, diastole_frame):
     diastole_slice_indexes = frame_slice_dict[diastole_frame]
     return np.percentile(np.array(diastole_slice_indexes), (55), interpolation='lower')
 
+
 def read_pathology(meta_txt):
     pathology = ""
     with open(meta_txt, "r") as f:
         pathology = f.readline().split(": ")[1]
-    if pathology == "":
-        exit(1)
     return pathology.rstrip()
 
+
 def resize_matrices(matrices):
-    return list(map(lambda x: cv.resize(x, (224,224), interpolation = cv.INTER_AREA) , matrices))
+    return list(map(lambda x: cv.resize(x, (224, 224), interpolation=cv.INTER_AREA), matrices))
+
 
 def create_pickle_for_patient(in_dir, out_dir):
     scan_id = os.path.basename(in_dir)
@@ -87,7 +93,11 @@ def create_pickle_for_patient(in_dir, out_dir):
         logger.error("Could not create pickle file for {}".format(scan_id))
         return
 
-    cr = CONreaderVM(con_file)
+    try:
+        cr = CONreaderVM(con_file)
+    except UnicodeDecodeError:
+        print('Difficult con file for {}'.format(con_file))
+        return
     contours = left_ventricle_contours(cr.get_hierarchical_contours())
 
     frame_slice_dict = collect_contour_slices_by_frames(contours)
@@ -102,10 +112,14 @@ def create_pickle_for_patient(in_dir, out_dir):
     mid_slice = calculate_mid_slice(frame_slice_dict, diastole_frame)
     hearth_cycle = []
     for i in range(0, dr.num_frames):
-        frame_ind = (i + systole_frame) % dr.num_frames
-        img = dr.get_image(mid_slice, frame_ind)
-        img *= 255 / img.max()
-        hearth_cycle.append(img.astype('uint8'))
+        try:
+            frame_ind = (i + systole_frame) % dr.num_frames
+            img = dr.get_image(mid_slice, frame_ind)
+            img *= 255 / img.max()
+            hearth_cycle.append(img.astype('uint8'))
+        except IndexError:
+            print('Index error for {}'.format(scan_id))
+            return
 
     pathology = read_pathology(meta_txt)
     hearth_cycle = resize_matrices(hearth_cycle)
@@ -114,6 +128,7 @@ def create_pickle_for_patient(in_dir, out_dir):
     with (open(pickle_file_path, "wb")) as pickleFile:
         pickle.dump(patient_data, pickleFile)
 
+
 in_dir = sys.argv[1]
 out_dir = sys.argv[2]
 
@@ -121,5 +136,9 @@ if not os.path.isdir(in_dir):
     logger.error("Invalid input directory: {}".format(in_dir))
 else:
     patient_folders = sorted(os.listdir(in_dir))
+    target_files_already_exists = set(os.listdir(out_dir))
     for patient_folder in patient_folders:
-        create_pickle_for_patient(os.path.join(in_dir, patient_folder), out_dir)
+        if not (patient_folder + '.p' in target_files_already_exists):
+            create_pickle_for_patient(os.path.join(in_dir, patient_folder), out_dir)
+        else:
+            print('Already done: {}'.format(patient_folder))
