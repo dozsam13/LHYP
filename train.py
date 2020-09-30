7b383532482b40af7e9c9dcd7f257823a67159ff
dozsam13 <dozsam13@gmail.com>
2020-09-30 16:40:00 +0200

    loss and accuracy calc

--- train.py
+++ train.py
@@ -10,14 +10,15 @@ import util.plot_util as plot_util
 from data_reader import DataReader
 from hypertrophy_classifier import HypertrophyClassifier
 from hypertrophy_dataset import HypertrophyDataset
+from datetime import datetime
 
 
-def calc_accuracy(loader, model):
+def calculate_accuracy(loader, model):
     correctly_labeled = 0
     for sample in loader:
-        image = sample['image']
+        sequence = sample['sequence']
         target = sample['target']
-        res = model(image)
+        res = model(sequence)
         predicted = torch.argmax(res, dim=1)
         correctly_labeled += torch.eq(target, predicted).sum().cpu().detach().numpy()
 
@@ -48,9 +49,9 @@ def calculate_loss(loader, model, criterion):
     counter = 0
     for sample in loader:
         counter += 1
-        image = sample['image']
+        sequence = sample['sequence']
         target = sample['target']
-        predicted = model(image)
+        predicted = model(sequence)
         loss = criterion(predicted, target)
         loss_sum += loss.cpu().detach().numpy()
         del loss
@@ -71,11 +72,19 @@ def train_model():
     (train_data, validation_data, test_data) = split_data(0.66, 0.83, data_reader.x, data_reader.y)
     dataset = HypertrophyDataset(train_data[0], train_data[1], device)
     loader_train = DataLoader(dataset, batch_size)
+    dataset = HypertrophyDataset(train_data[0], train_data[1], device)
+    loader_train_accuracy = DataLoader(dataset, batch_size)
+    dataset = HypertrophyDataset(validation_data[0], validation_data[1], device)
+    loader_validation = DataLoader(dataset, batch_size)
 
-    epochs = 15
+    epochs = 3
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
     train_losses = []
+    train_accuracies = []
+    dev_losses = []
+    dev_accuracies = []
+    print("Training has started at {}".format(datetime.now()))
     for epoch in range(epochs):
         trainloss_for_epoch = 0.0
         counter = 0
@@ -91,8 +100,13 @@ def train_model():
             counter += 1
         trainloss_for_epoch /= counter
         train_losses.append(trainloss_for_epoch)
+        train_accuracies.append(calculate_accuracy(loader_train_accuracy, model))
+        dev_losses.append(calculate_loss(loader_validation, model, criterion))
+        dev_accuracies.append(calculate_accuracy(loader_validation, model))
+    print("Training has finished at {}".format(datetime.now()))
 
-    plot_util.plot_data(train_losses, 'train_loss', train_losses, 'dev_loss', "loss.png")
+    plot_util.plot_data(train_losses, 'train_loss', dev_losses, 'dev_loss', "loss.png")
+    plot_util.plot_data(train_accuracies, 'train accuracy', dev_accuracies, 'dev accuracy', "accuracy.png")
 
 if __name__ == '__main__':
     train_model()
